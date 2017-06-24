a23716e494a5f90bf0c52fbcf1a3be0444bd6f93
eddietang4253 <eddietang4253@gmail.com>
2017-06-24 15:58:52 +0800

    <Snap picture and store it >

---
+++ src/pages/Main.js
@@ -0,0 +1,73 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+import * as firebase from 'firebase';  // Initialize Firebase
+  var fireBaseconfig = {
+    apiKey: "Ypur apiKey",
+    authDomain: "your auth domain",
+    databaseURL: "your db url",
+    storageBucket: "your storageBucket",
+    messagingSenderId: "your id"
+  };
+  // firebase.initializeApp(fireBaseconfig);
+const firebaseApp = firebase.initializeApp(fireBaseconfig);
+
+export default class snapfunc extends Component {
+  render() {
+    return (
+          <View style={styles.container}>
+            <Camera
+              ref={(cam) => {
+                this.camera = cam;
+              }}
+              style={styles.preview}
+              aspect={Camera.constants.Aspect.fill}>
+              <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+            </Camera>
+          </View>
+        );
+      }
+
+  takePicture() {
+      const options = {};
+      //options.location = ...
+      this.camera.capture({metadata: options})
+        .then((data) => console.log(data))
+        .catch(err => console.error(err));
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  }
+});
+
+AppRegistry.registerComponent('snapfunc', () => snapfunc);
